1682e5f97d9b057fbd3a6bd5a745342cc25a1b72
ALBANO Daniel <deanidev@gmail.com>
2022-09-15 14:01:47 +0200

    wip

--- index.php
+++ index.php
@@ -5,17 +5,20 @@ for ($i = 0; $i <= 41; $i++) {
     $calandrier[$i] = '';
 }
 
-$choosedYear = $_GET['year'];
-$choosedMonth = $_GET['month'];
+$currentYear = Date('Y');
+$currentMonth = Date('n');
 
-$choosedDate = new DateTime("$choosedYear-$choosedMonth-1"); 
+$choosedYear = $_GET['year'] ?? $currentYear;
+$choosedMonth = $_GET['month'] ?? $currentMonth;
+
+$choosedDate = new DateTime("$choosedYear-$choosedMonth-1");
 // could future add $choosedDay || it would be related to JS effects -> affection to object
 
 $choosedMonthLiteral = $choosedDate->format('F');
 $choosedMonthTotalDays = $choosedDate->format('t'); // 28-31
 $choosedMonthFirstWeekDay = $choosedDate->format('N'); // 1 for monday, 7 for sunday
 
-//a for to fill the actual month days
+//an for to fill the actual month days i.e. $calandrier[]
 $dayCounter = $choosedMonthFirstWeekDay - 1;
 for ($i = 1; $i <= $choosedMonthTotalDays; $i++) {
     $calandrier[$dayCounter] = $i;
@@ -35,10 +38,14 @@ for ($i = 1; $i <= $choosedMonthTotalDays; $i++) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Inline&display=swap" rel="stylesheet">
     <title>Calendar</title>
 </head>
 
 <body>
+    <h1>Calendar: choose a date</h1>
     <form action="./" method="get">
         <label for="month">Month :</label>
         <select name="month" id="month">
@@ -59,6 +66,10 @@ for ($i = 1; $i <= $choosedMonthTotalDays; $i++) {
         <input type="number" name="year" id="year" min="1000" max="3000" required>
         <input type="submit" value="Search!">
     </form>
+    <div>
+        <form action="get"><input type="number" value="12" name="ok"></form>
+    </div>
+    <!-- CALENDAR TABLE  -->
     <table>
         <caption><?= $choosedMonthLiteral . ' - ' . $choosedYear ?></caption>
         <thead>
@@ -73,43 +84,24 @@ for ($i = 1; $i <= $choosedMonthTotalDays; $i++) {
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <?php
-                for ($i = 0; $i <= 6; $i++) { ?>
-                    <td><?= $calandrier[$i] ?></td>
-                <?php } ?>
-            </tr>
-            <tr>
-                <?php
-                for ($i = 7; $i <= 13; $i++) { ?>
-                    <td><?= $calandrier[$i] ?></td>
-                <?php } ?>
-            </tr>
-            <tr>
             <?php
-                for ($i = 14; $i <= 20; $i++) { ?>
-                    <td><?= $calandrier[$i] ?></td>
-                <?php } ?>
-            </tr>
-            <tr>
-            <?php
-                for ($i = 21; $i <= 27; $i++) { ?>
-                    <td><?= $calandrier[$i] ?></td>
-                <?php } ?>
-            </tr>
-            <tr>
-            <?php
-                for ($i = 28; $i <= 34; $i++) { ?>
-                    <td><?= $calandrier[$i] ?></td>
-                <?php } ?>
-            </tr>
-            <tr>
-            <?php
-                for ($i = 35; $i <= 41; $i++) { ?>
-                    <td><?= $calandrier[$i] ?></td>
-                <?php } ?>
-            </tr>
+            $i = 0;
+            $index = 0;
+            while ($i <= 41) {
+                if ($i % 7 == 0) echo '<tr>';
+                $i++;
+                while ($i % 7 != 0) {
+                    echo '<td>' . $calandrier[$index] . '</td>';
+                    $index++;
+                    $i++;
+                };
+                echo '<td>' . $calandrier[$index] . '</td>';
+                $index++;
+                echo '</tr>';
+            };
+            ?>
         </tbody>
     </table>
 </body>
+
 </html>
